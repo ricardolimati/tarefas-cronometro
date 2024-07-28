23e32058c285239ea064e87e0cbb5a5d32e51f0c
ricardolimati <ricardodreamsites@gmail.com>
2024-07-28 15:48:16 -0300

    Projeto concluído

---
+++ src/components/Formulario/index.tsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import Button from "../Button";
+import style from "./Formulario.module.scss";
+import { Itarefa } from "../../types/tarefa";
+import { v4 as uuidv4 } from 'uuid';
+
+interface Props {
+  setTarefas: React.Dispatch<React.SetStateAction<Itarefa[]>>
+}
+
+function Formulario({ setTarefas }: Props) {
+  const [tarefa, setTarefa] = useState("");
+  const [tempo, setTempo] = useState('00:00');
+
+  function adicionarTarefa(evento: React.FormEvent<HTMLFormElement>) {
+    evento.preventDefault();
+    setTarefas(tarefasAntigas =>
+      [
+        ...tarefasAntigas,
+        {
+          tarefa,
+          tempo,
+          selecionado: false,
+          completado: false,
+          id: uuidv4()
+        }
+      ]
+    );
+    setTarefa("");
+    setTempo("00:00");
+  }
+  return (
+    <form className={style.novaTarefa} onSubmit={adicionarTarefa}>
+      <div className={style.inputContainer}>
+        <label htmlFor={style.tarefa}>
+          Adicione uma novo estudo
+        </label>
+        <input type="text"
+          name="tarefa"
+          id="tarefa"
+          value={tarefa}
+          onChange={evento => setTarefa(evento.target.value)}
+          placeholder="O que você quer estudar"
+          required
+        />
+      </div>
+      <div className={style.inputContainer}>
+        <label htmlFor="">
+          Tempo
+        </label>
+        <input
+          type="time"
+          step="1"
+          name="tempo"
+          value={tempo}
+          onChange={evento => setTempo(evento.target.value)}
+          id="tempo"
+          min="00:00:00"
+          max="01:30:00"
+          required
+        />
+
+      </div>
+      <Button type="submit">
+        Adicionar
+      </Button>
+
+    </form>
+  );
+}
+
+export default Formulario;
